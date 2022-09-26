ad62f8402f5951c138d8e444cf23151d1a82d021
Youkass <youribougre@gmail.com>
2022-09-26 13:44:38 +0200

    adding define in includes.h

--- includes/includes.h
+++ includes/includes.h
@@ -6,13 +6,18 @@
 /*   By: denissereno <denissereno@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 13:31:18 by denissereno       #+#    #+#             */
-/*   Updated: 2022/09/26 13:31:50 by denissereno      ###   ########.fr       */
+/*   Updated: 2022/09/26 13:44:22 by yobougre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INCLUDES_H
 # define INCLUDES_H
 
-#include <stdio.h>
+# include <stdio.h>
+# include <math.h>
+
+# define PI 3.14159 
+# define P2 PI/2
+# define P3 3*PI/2
 
 #endif
