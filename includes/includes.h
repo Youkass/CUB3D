e7f70266ccb5a62659961252a68e74092fe897dd
Youkass <youribougre@gmail.com>
2022-09-26 16:00:31 +0200

    adding mlx_utils repo and mlx_utils function

--- includes/includes.h
+++ includes/includes.h
@@ -6,7 +6,7 @@
 /*   By: denissereno <denissereno@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 13:31:18 by denissereno       #+#    #+#             */
-/*   Updated: 2022/09/26 14:08:12 by yobougre         ###   ########.fr       */
+/*   Updated: 2022/09/26 14:34:28 by yobougre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@
 # define PI 3.14159 
 # define P2 PI/2
 # define P3 3*PI/2
+# define WIN_H 1080
+# define WIN_W 1920
+# define PRG_NAME "Cub3d by Curly Squad"
 
 #endif
--- includes/struct.h
+++ includes/struct.h
@@ -6,22 +6,39 @@
 /*   By: denissereno <denissereno@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 13:30:30 by denissereno       #+#    #+#             */
-/*   Updated: 2022/09/26 13:31:06 by denissereno      ###   ########.fr       */
+/*   Updated: 2022/09/26 15:16:59 by yobougre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUC_H
 # define STRUC_H
 
-typedef struct s_vector2D {
+typedef struct s_vector2D
+{
     int x;
     int y;
 }   t_vector2D;
 
-typedef struct s_vector3D {
+typedef struct s_vector3D
+{
     int x;
     int y;
     int z;
 }   t_vector3D;
+
+typedef struct	s_mlx
+{
+	void	*mlx;
+	void	*mlx_win;
+}	t_mlx;
+
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
 
 #endif
