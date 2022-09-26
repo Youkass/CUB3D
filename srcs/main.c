680e407b240e1901e7eca031b95196920597e3c7
Youkass <youribougre@gmail.com>
2022-09-26 15:59:52 +0200

    minor changes

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: denissereno <denissereno@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 13:29:24 by denissereno       #+#    #+#             */
-/*   Updated: 2022/09/26 14:10:48 by yobougre         ###   ########.fr       */
+/*   Updated: 2022/09/26 15:59:30 by yobougre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int main(int argc, char **argv)
 {
+	t_mlx	data;
+
     if (argc != 2)
     {
         printf("Arguments error\n");
