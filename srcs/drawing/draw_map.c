4c4fef52fef3faa8afe62e1dddb7268c258300c0
Darian Sereno <dasereno@student.42.fr>
2022-09-29 17:20:42 +0200

    [FIX] displaying player

--- srcs/drawing/draw_map.c
+++ srcs/drawing/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: denissereno <denissereno@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 13:26:11 by yobougre          #+#    #+#             */
-/*   Updated: 2022/09/29 17:08:44 by yobougre         ###   ########.fr       */
+/*   Updated: 2022/09/29 17:19:49 by denissereno      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,7 +122,8 @@ void	ft_draw_floor(t_obj wall)
 
 void	ft_draw_map(void)
 {
-	t_int	var;
+	t_int		var;
+	t_vector2D	p_pos;
 
 	var.i = 0;
 	ft_malloc_map();
@@ -131,18 +132,17 @@ void	ft_draw_map(void)
 		var.j = 0;
 		while (var.j < _img()->map_width)
 		{
-			if (_img()->coord_map[var.i][var.j].id == PLAYER)
-			{	
-				ft_draw_floor(_img()->coord_map[var.i][var.j]);
-				ft_draw_player(_img()->coord_map[var.i][var.j]);
-			}
-			else if (_img()->coord_map[var.i][var.j].id == WALL)
+			if (_img()->coord_map[var.i][var.j].id == WALL)
 				ft_draw_wall(_img()->coord_map[var.i][var.j]);
-			else if (_img()->coord_map[var.i][var.j].id == MAP)
+			if (_img()->coord_map[var.i][var.j].id == MAP)
 				ft_draw_floor(_img()->coord_map[var.i][var.j]);
+			if (_img()->coord_map[var.i][var.j].id == PLAYER)
+				p_pos = (t_vector2D){var.j, var.i};
 			var.j++;
 		}
 		var.i++;
 	}
+	ft_draw_floor(_img()->coord_map[p_pos.y][p_pos.x]);
+	ft_draw_player(_img()->coord_map[p_pos.y][p_pos.x]);
 	mlx_put_image_to_window(_mlx()->mlx, _mlx()->mlx_win, _img()->img, 0, 0);
 }
