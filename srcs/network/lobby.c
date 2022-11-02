d2167d98e267e7bad64c838ac9191301672ab8e1
Darian Sereno <dasereno@student.42.fr>
2022-11-02 19:50:20 +0100

    [FIX] Network lobby working

--- srcs/network/lobby.c
+++ srcs/network/lobby.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   lobby.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: denissereno <denissereno@student.42.fr>    +#+  +:+       +#+        */
+/*   By: dasereno <dasereno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 14:24:08 by denissereno       #+#    #+#             */
-/*   Updated: 2022/11/02 14:08:55 by yobougre         ###   ########.fr       */
+/*   Updated: 2022/11/02 17:36:02 by dasereno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub.h"
 
-static int	ft_has_recv(t_client_thread *client, int nb)
+int	ft_has_recv(t_client_thread *client, int nb)
 {
 	int	i;
 
@@ -28,7 +28,7 @@ static int	ft_has_recv(t_client_thread *client, int nb)
 	return (0);
 }
 
-static int	ft_has_sent(t_client_thread *client, int nb)
+int	ft_has_sent(t_client_thread *client, int nb)
 {
 	int	i;
 
@@ -44,7 +44,7 @@ static int	ft_has_sent(t_client_thread *client, int nb)
 	return (0);
 }
 
-static void	ft_can_start(t_client_thread *client, int nb)
+void	ft_can_start(t_client_thread *client, int nb)
 {
 	int	i;
 
@@ -63,7 +63,7 @@ static void	ft_can_start(t_client_thread *client, int nb)
 	pthread_mutex_unlock(client->mutex);
 }
 
-static int	ft_has_start(t_client_thread *client, int nb)
+int	ft_has_start(t_client_thread *client, int nb)
 {
 	int	i;
 
@@ -80,64 +80,64 @@ static int	ft_has_start(t_client_thread *client, int nb)
 	return (0);
 }
 
-int	ft_recv_first_data_lobby(t_client_thread *client, int nb)
+int	ft_recv_first_data_lobby(t_client_thread *client)
 {
-	pthread_mutex_lock(client->mutex);
-	if (recv(client->socket, &(client->player_data), 
-				sizeof(t_obj), 0) < 0)
+	t_send_client	cl;
+
+	if (recv(client->socket, &(cl), sizeof(t_send_client), 0) < 0)
 		return (pthread_mutex_unlock(client->mutex), 1);
+	pthread_mutex_lock(client->mutex);
+	client->player_data = cl.player;
 	client->serv->player_data[client->id] = client->player_data;
-	client->is_send = 0;
-	client->is_recv = 1;
+	if (cl.start == 1)
+		client->serv->started = 1;
+	// nb = client->serv->linked_players;
 	pthread_mutex_unlock(client->mutex);
-	while (ft_has_recv(client, nb))
-	{}
+	// while (ft_has_recv(client, nb))
+	// {}
 	return (0);
 }
 
-int	ft_send_all_data_lobby(t_client_thread *client, int nb)
+int	ft_send_all_data_lobby(t_client_thread *client)
 {
-	t_obj	o_player[MAX_PLAYER];
+	int				nb;
+	t_send_server	pl;
 	int		i;
 
 	i = 0;
-	ft_can_start(client, nb);
-	memset(&o_player, 0, sizeof(o_player));
+	memset(&pl.player, 0, sizeof(pl.player));
+	memset(&pl, 0, sizeof(pl));
 	pthread_mutex_lock(client->mutex);
+	nb = client->serv->linked_players;
 	while (i < nb)
 	{
-		o_player[i] = client->serv->player_data[i];
+		pl.player[i] = client->serv->player_data[i];
 		++i;
 	}
-	if (send(client->socket, &o_player, sizeof(o_player), 0) < 0)
+	pl.start = 0;
+	if (client->serv->started == 1)
+		pl.start = 1;
+	pl.linked_pl = nb;
+	if (send(client->socket, &pl, sizeof(pl), 0) < 0)
 		return (pthread_mutex_unlock(client->mutex), 1);
-	client->is_send = 1;
-	pthread_mutex_unlock(client->mutex);
-	while (ft_has_sent(client, nb))
-	{}
-	pthread_mutex_lock(client->mutex);
-	client->is_recv = 0;
 	pthread_mutex_unlock(client->mutex);
 	return (0);
 }
 
 int	wait_lobby(t_client_thread *client)
 {
-	int	nb;
-
 	while (1)
 	{
-		pthread_mutex_lock(client->mutex);
-		nb = client->serv->linked_players;
-		pthread_mutex_unlock(client->mutex);
-		if (send(client->socket, &nb, sizeof(int), 0) < 0)
+		// if (send(client->socket, &nb, sizeof(int), 0) < 0)
+		// 	return (1);
+		if (ft_recv_first_data_lobby(client))
 			return (1);
-		if (ft_recv_first_data_lobby(client, nb))
+		if (ft_send_all_data_lobby(client))
 			return (1);
-		if (ft_send_all_data_lobby(client, nb))
-			return (1);
-		if (!ft_has_start(client, nb))
-			return (printf("je sors bien de wait_lobby\n"), 0);
+		if (client->serv->started == 1)
+			break;
+		// if (!ft_has_start(client, nb))
+		// 	return (printf("je sors bien de wait_lobby\n"), 0);
 	}
 	return (0);
 }
