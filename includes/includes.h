f9b789e2de31fb4e018ef49158f9b819ad401eb3
Darian Sereno <dasereno@student.42.fr>
2022-11-12 12:45:07 +0100

    oui

--- includes/includes.h
+++ includes/includes.h
@@ -6,7 +6,7 @@
 /*   By: denissereno <denissereno@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 13:31:18 by denissereno       #+#    #+#             */
-/*   Updated: 2022/11/12 12:04:30 by yobougre         ###   ########.fr       */
+/*   Updated: 2022/11/12 12:45:00 by denissereno      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ esc : 65307
 # define TRED 0
 # define TBLUE 1
 
-# define NB_ROUNDS 1
+# define NB_ROUNDS 5
 
 # define GAME 0
 # define MENU 1
