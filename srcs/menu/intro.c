46874db52b64f9fc5945e4bbb7046cad55292ef7
Youkass <youribougre@gmail.com>
2022-11-16 15:55:05 +0100

    [ISSUE] can't find why Darian's sprites are not displayed, gonna try to finish it later

--- srcs/menu/intro.c
+++ srcs/menu/intro.c
@@ -6,7 +6,7 @@
 /*   By: yuro4ka <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 01:57:27 by yuro4ka           #+#    #+#             */
-/*   Updated: 2022/11/16 02:36:42 by yobougre         ###   ########.fr       */
+/*   Updated: 2022/11/16 15:54:15 by yobougre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	ft_open_intro_sprites(void)
 	_image()->intro_sprite[5] = generate_image("./img/intro/youri_3.xpm");
 	_image()->intro_sprite[6] = generate_image("./img/intro/us_1.xpm");
 	_image()->intro_sprite[7] = generate_image("./img/intro/us_2.xpm");
-	printf("NTM\n");
 }
 
 void	ft_intro_sprite_to_image(int i1, int i2, t_vector2D v1, t_vector2D v2)
@@ -33,30 +32,94 @@ void	ft_intro_sprite_to_image(int i1, int i2, t_vector2D v1, t_vector2D v2)
 		_image()->intro_sprite[i2], v2, (t_vector2F){0.5, 0.5});
 }
 
+void	ft_draw_youri(int limite)
+{
+	static t_vector2D		pos;
+	static unsigned long	start = 0;
+	static unsigned long	clock = 0;
+	static int				my_limite = 0;
+
+	if (!start)
+	{
+		pos.x = 0 - _image()->intro_sprite[3].w;
+		pos.y = 100;
+		clock = get_clock(_var()->clock);
+		start = get_clock(_var()->clock);
+		my_limite = limite - (_image()->intro_sprite[3].w / 2);
+	}
+	if (pos.x < my_limite)
+		ft_put_image_to_image_scale(*_img(),
+			_image()->intro_sprite[3], pos, (t_vector2F){0.5, 0.5});
+	else if (pos.x >= my_limite && pos.x < my_limite * 2)
+		ft_put_image_to_image_scale(*_img(),
+			_image()->intro_sprite[4], pos, (t_vector2F){0.5, 0.5});
+	else if (pos.x >= my_limite * 2 && pos.x <= my_limite * 3)
+		ft_put_image_to_image_scale(*_img(),
+			_image()->intro_sprite[5], pos, (t_vector2F){0.5, 0.5});
+	if (get_time(clock) > 10000)
+	{
+		++pos.x;
+		clock = get_clock(_var()->clock);
+	}
+}
+
+void	ft_draw_darian(int limite)
+{
+	static t_vector2D		pos;
+	static unsigned long	start = 0;
+	static unsigned long	clock = 0;
+	static int				my_limite = 0;
+	
+	(void)my_limite;
+	if (!start)
+	{
+		pos.x = WIN_W;
+		pos.y = 100;
+		clock = get_clock(_var()->clock);
+		start = get_clock(_var()->clock);
+		my_limite = limite + (_image()->intro_sprite[3].w / 2);
+	}
+//	if (get_time(start) < 10000000)
+		ft_put_image_to_image_scale(*_img(),
+			_image()->intro_sprite[1], pos, (t_vector2F){0.5, 0.5});
+	/*
+	else if (pos.x <= my_limite & pos.x > my_limite * 2)
+		ft_put_image_to_image_scale(*_img(),
+			_image()->intro_sprite[1], pos, (t_vector2F){0.5, 0.5});
+	else if (pos.x <= my_limite * 2 && pos.x >= my_limite * 3)
+		ft_put_image_to_image_scale(*_img(),
+			_image()->intro_sprite[2], pos, (t_vector2F){0.5, 0.5});*/
+	if (get_time(clock) > 10000)
+	{
+		--pos.x;
+		clock = get_clock(_var()->clock);
+	}
+}
+
 void	ft_intro(void)
 {
 	static unsigned long	start = 0;
-	static unsigned long	check = 0;
-	static t_vector2D		double_pos[2];
-	int						limite;
+	/*static unsigned long	check = 0;
+	static t_vector2D		double_pos[2];*/
+	static int				limite;
 
 	if (!start)
 	{
 		start = get_clock(_var()->clock);
 		ft_open_intro_sprites();
-		double_pos[0].x = -100;
-		double_pos[0].y = 240;
-		double_pos[1].x = WIN_W + 100; 
-		double_pos[1].y = 240;
+	/*	double_pos[0].x = -100;
+		double_pos[0].y = 100;
+		double_pos[1].x = WIN_W; 
+		double_pos[1].y = 100;*/
 		limite = (WIN_W / 8);
+		printf("limite :%d\n", limite);
 	}
-	if (double_pos[0].x < limite)
-		ft_intro_sprite_to_image(3, 0, double_pos[0], double_pos[1]);
-	else if (double_pos[0].x > limite && double_pos[0].x < limite * 2)
-		ft_intro_sprite_to_image(4, 1, double_pos[0], double_pos[1]);
-	else if (double_pos[0].x > limite * 2 && double_pos[0].x < limite * 3)
-		ft_intro_sprite_to_image(5, 2, double_pos[0], double_pos[1]);
-	else
+	draw_bg((char [4]){0, 0, 0, 0});
+	ft_put_image_to_image(*_img(), _menu()->bg,
+		(t_vector2D){0, 0});
+	ft_draw_darian(limite);
+	ft_draw_youri(limite);
+	/*else
 	{
 		double_pos[0].x = WIN_W / 2 - _image()->intro_sprite[6].w ;
 		if (!check)
@@ -67,11 +130,5 @@ void	ft_intro(void)
 		else
 			ft_put_image_to_image_scale(*_img(),
 				_image()->intro_sprite[7], double_pos[0], (t_vector2F){0.5, 0.5});
-	}
-	if (get_time(start) > 10000)
-	{
-		++double_pos[0].x;
-		--double_pos[1].x;
-		start = get_clock(_var()->clock);
-	}
+	}*/
 }
